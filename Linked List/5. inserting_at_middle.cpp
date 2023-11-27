ddede108e095c91f4df8e0028ec6e6ba3e929050
eliasdebelo <eliascs2012@gmail.com>
2023-11-27 11:28:22 +0300

    insertng node to  middle

---
+++ Linked List/5. inserting_at_middle.cpp
@@ -0,0 +1,37 @@
+https://www.zoosk.com/date-mix/relationship-advice/healthy-relationships/how-to-build-sexual-tension/
+
+BaaS
+1. https://www.back4app.com/
+2. https://parseplatform.org/
+3. https://firebase.google.com/
+4. https://backendless.com/
+5. https://aws.amazon.com/amplify/
+
+
+
+https://docs.flutter.dev/deployment/android#enabling-material-components
+https://pub.dev/documentation/youtube_explode_dart/latest/
+
+https://stackoverflow.com/questions/54122850/how-to-read-and-write-a-text-file-in-flutter
+https://docs.flutter.dev/cookbook/persistence/reading-writing-files
+
+https://www.projectsof8051.com/agricultural-electronics-projects/
+
+
+Upwork.#321
+
+qubeetpcom_attend
+oeV^Tu-u7vNq
+---------------------------
+Custom Data Collection Tool
+---------------------------
+HU Staffs
+================================
+Updates
+	route
+		attendance_type
+	page
+		attendance_type
+	home link to 
+		home coontroller - chooseType
+		attendance_type
